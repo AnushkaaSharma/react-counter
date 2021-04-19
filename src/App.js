86cb901a51f2eec0b37f528c8fece65b16137ddf
AnushkaaSharma <snaushkaa4@gmail.com>
2021-04-19 21:09:36 +0545

    counter using states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0
+  }
+  increment = () => {
+    this.setState({ count: this.state.count+1 })
+  }
+
+  decrement = () => {
+    this.setState ({ count: this.state.count-1 })
+  }
+
+  reset = () => {
+    this.setState ({ count: 0 })
+  }
+  render () {
+    return (
+      <div>
+        <p>{ this.state.count }</p>
+        <button onClick={ this.increment }>Increment</button>
+        <button onClick={ this.decrement }>Decrement</button>
+        <button onClick={ this.reset }>Reset</button>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
