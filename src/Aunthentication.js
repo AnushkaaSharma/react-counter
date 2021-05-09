dc563899e343446cee58082574cf929d355852e4
AnushkaaSharma <snaushkaa4@gmail.com>
2021-05-09 22:42:17 +0545

    authentication portal

---
+++ src/Aunthentication.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Aunthentication() {
+    return (
+        <div className='authentication'>
+            <h1>Authentication Portal</h1>
+            
+        </div>
+    )
+}
